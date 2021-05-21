cdc83c6875c6057edd643bb6d489215900ebdb25
sushilv3 <sushilv3rma@gmail.com>
2021-05-21 10:51:52 +0530

    delete unnecessary line or comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Todo from './components/Todo';
 import './App.css';
 
 const  App = () =>{
   return (<>
-    
+    <Todo />
     <h2>App component</h2>
     </>
 
