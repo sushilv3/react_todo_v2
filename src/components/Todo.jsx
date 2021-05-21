0d1d674d80ee5826b2c8aa4e8de129829afb3ac1
sushilv3 <sushilv3rma@gmail.com>
2021-05-21 11:56:10 +0530

    added edit functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,31 +1,60 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTrashAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const Todo = () => {
-    const[inputData,setInputData] = useState();
-const [items,setItems] = useState([]);
+  //accept input from input element
+  const [inputData, setInputData] = useState();
 
-    const addItem = ()=>{
-        if(!inputData){
-           
-        }else{
-            setItems([...items,inputData]);
-            setInputData('');
-        }
+  //show array of items
+  const [items, setItems] = useState([]);
+
+  const [toggleBtn, setToggleBtn] = useState(true);
+
+  const [updateEditItem, setUpdateEditItem] = useState("");
+
+  const addItem = () => {
+    if (!inputData) {
+        alert('pls fill data');
+    } else if (inputData && !toggleBtn) {
+        setItems(items.map((elem)=>{
+            if(elem.id === setUpdateEditItem){
+                return{...elem, name:inputData}
+            }
+            return elem;
+        }));
+    }else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
+      setInputData("");
+    }
+  };
+
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((val) => {
+      return index !== val.id;
+    });
+    setItems(updatedItems);
+  };
+
+  const removeAll = () => {
+    setItems([]);
+  };
+
+  const editItem = (id) => {
+    let newEditItem = items.find((ele) => {
+      return ele.id === id;
+    });
+    setToggleBtn(false);
     
-    }
+    setInputData(newEditItem.name);
 
-    const deleteItem = (id)=>{
-        const updatedItems = items.filter((val,ind)=>{
-            return ind !== id;
-        }); 
-        setItems(updatedItems);
-    }
+    setUpdateEditItem(id);
 
-    const removeAll =()=>{
-        setItems([]);
-    }
+  };
 
   return (
     <>
@@ -36,23 +65,40 @@ const [items,setItems] = useState([]);
             <p>Add your List here</p>
           </div>
           <div className="additems">
-            <input type="text" placeholder="add items" 
-            value={inputData} onChange={(e)=>setInputData(e.target.value)}
+            <input
+              type="text"
+              placeholder="add items"
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
             />
-            <FontAwesomeIcon icon={faPlus}  onClick={addItem}/>
+            {toggleBtn ? (
+              <FontAwesomeIcon icon={faPlus} onClick={addItem} />
+            ) : (
+              <FontAwesomeIcon
+                title="Update Item"
+                icon={faEdit}
+                onClick={addItem}
+              />
+            )}
           </div>
           <div className="show-items">
-          {items.map((val,index)=>{
-              return(
-                <div className="item" key={index}>
-                <h3>{val}</h3>
-                <FontAwesomeIcon icon={faTrashAlt} onClick={()=>deleteItem(index)}/>
-              </div>
+            {items.map((val, index) => {
+              return (
+                <div className="item" key={val.id}>
+                  <h3>{val.name}</h3>
+                  <FontAwesomeIcon
+                    title="Delete Item"
+                    icon={faTrashAlt}
+                    onClick={() => deleteItem(val.id)}
+                  />
+                  <FontAwesomeIcon
+                    title="Edit Item"
+                    icon={faEdit}
+                    onClick={() => editItem(val.id)}
+                  />
+                </div>
               );
-          })
-        }
-          
-         
+            })}
           </div>
           <div className="">
             <button onClick={removeAll}>
